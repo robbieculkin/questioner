6b5152d40265c52bfc7548c567613fcf87f3d3f0
Tim Shur <tim.shur@gmail.com>
2019-01-14 19:22:55 -0800

    frontend: [Card] update styling

--- frontend/src/components/Card/index.js
+++ frontend/src/components/Card/index.js
@@ -6,10 +6,10 @@ import Entry from '../Entry';
 const Card = ({ history, onTextEntry }) =>
   <div className='card'>
     <div className='card-item'>
-        <div className='question'>
-          {history[history.length - 1].text}
-        </div>
-        <Entry onTextEntry={onTextEntry} />
+      <div className='question'>
+        {history[history.length - 1].text}
+      </div>
+      <Entry onTextEntry={onTextEntry} />
     </div>
   </div>
 
