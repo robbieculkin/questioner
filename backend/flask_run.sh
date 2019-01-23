2268f018fb42d3dd3f2d71c4a3697289b3cee648
Tim Shur <tim.shur@gmail.com>
2019-01-22 16:01:53 -0800

    backend: [flask] add basic flask api endpoint

---
+++ backend/flask_run.sh
@@ -0,0 +1,3 @@
+export FLASK_APP=flaskr
+export FLASK_ENV=development
+python3 -m flask run
---
+++ backend/flaskr/__init__.py
@@ -0,0 +1,39 @@
+import os
+import json
+
+from flask import Flask
+from flask_cors import CORS
+
+
+def create_app(test_config=None):
+    # create and configure the app
+    app = Flask(__name__, instance_relative_config=True)
+    CORS(app)
+    app.config.from_mapping(
+        SECRET_KEY='dev',  # override for deployment to some random value
+        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
+    )
+
+    if test_config is None:
+        # load the instance config, if it exists, when not testing
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        # load the test config if passed in
+        app.config.from_mapping(test_config)
+
+    # ensure the instance folder exists
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    # a simple page that says hello
+    @app.route('/api/v0/question')
+    def hello():
+        data = {
+            'id': 9,
+            'question': 'Who are you?'
+        }
+        return json.dumps(data)
+
+    return app
