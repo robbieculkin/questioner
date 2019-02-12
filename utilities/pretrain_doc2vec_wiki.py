b5a2ba67867394d9750effb586bc4377356542ec
robbieculkin <robbieculkin@gmail.com>
2019-02-11 18:01:33 -0800

    [utilities] pretrain_doc2vec_wiki.py prints

--- utilities/pretrain_doc2vec_wiki.py
+++ utilities/pretrain_doc2vec_wiki.py
@@ -2,15 +2,11 @@ import multiprocessing
 from gensim.models.doc2vec import Doc2Vec, TaggedDocument
 from gensim.corpora.wikicorpus import WikiCorpus
 
-def fprint(msg):
-    with open('log/log.txt', 'w+') as fp:
-        fp.write(str(msg) + '\n')
-
-fprint('START SCRIPT')
+print('START SCRIPT')
 cores = multiprocessing.cpu_count() - 1
-fprint(f'USING CORES: {cores}')
+print(f'USING CORES: {cores}')
 wikipedia = WikiCorpus('/local/weka/enwiki-latest-pages-articles.xml.bz2')
-fprint('CORPUS FORMED')
+print('CORPUS FORMED')
 
 #build iterator
 class TaggedWikiDocument(object):
@@ -22,7 +18,7 @@ class TaggedWikiDocument(object):
             yield TaggedDocument(content, [title])
 
 documents = TaggedWikiDocument(wikipedia)
-fprint('TAGGED DOCUMENT')
+print('TAGGED DOCUMENT')
 
 models = [
     # PV-DBOW
@@ -30,15 +26,15 @@ models = [
     # PV-DM w/average
     Doc2Vec(dm=1, dm_mean=1, vector_size=200, window=8, min_count=19, epochs=10, workers=cores),
 ]
-fprint('CREATED MODELS')
+print('CREATED MODELS')
 
 models[0].build_vocab(documents)
-fprint(str(models[0]))
+print(str(models[0]))
 models[1].reset_from(models[0])
-fprint(str(models[1]))
+print(str(models[1]))
 
 for i, model in enumerate(models[1:]):
-    fprint(f'TRAINING MODEL #{i}')
+    print(f'TRAINING MODEL #{i}')
     model.train(documents, total_examples=model.corpus_count, epochs=model.iter)
-    fprint(f'SAVING MODEL #{i}')
+    print(f'SAVING MODEL #{i}')
     model.save(f'model_{i}')
