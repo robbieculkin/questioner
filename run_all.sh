7e5355a43e034fe5990b493c77eb7e99504d6ace
tshur <tim.shur@gmail.com>
2019-02-15 22:35:13 -0800

    run_all: add optional --dbpath argument for mongodb

--- run_all.sh
+++ run_all.sh
@@ -3,10 +3,31 @@
 # Subprocesses spawned here will be killed if parent is killed
 trap "trap - SIGTERM && kill -- -$$" SIGINT SIGTERM EXIT
 
+# Handle possible space-separated `--option argument
+DBPATH="/data/db"
+POSITIONAL=()
+while [[ $# -gt 0 ]]
+do
+key="$1"
+
+case $key in
+    --dbpath)
+    DBPATH="$2"
+    shift
+    shift
+    ;;
+    *)
+    POSITIONAL+=("$1")
+    shift
+    ;;
+esac
+done
+set -- "${POSITIONAL[@]}"
+
 mkdir -p log/
 
 # Start mongod
-mongod >log/mongod.log 2>&1 &
+mongod --dbpath $DBPATH >log/mongod.log 2>&1 &
 
 # Start flask server backend
 ./run_flask.sh 1>log/flask_out.log 2>log/flask_err.log &
