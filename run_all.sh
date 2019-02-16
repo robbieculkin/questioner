f95a44a0c265a560901b3ff97c1085e354823d76
tshur <tim.shur@gmail.com>
2019-02-15 21:25:38 -0800

    run_all: add script to run entire app with logging to file

---
+++ run_all.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Subprocesses spawned here will be killed if parent is killed
+trap "trap - SIGTERM && kill -- -$$" SIGINT SIGTERM EXIT
+
+mkdir -p log/
+
+# Start mongod
+mongod >log/mongod.log 2>&1 &
+
+# Start flask server backend
+cd backend
+./run_flask.sh 1>../log/flask_out.log 2>../log/flask_err.log &
+cd ..
+
+# Start Node/React frontend
+cd frontend
+npm start 1>/dev/null 2>../log/npm_err.log &
+cd ..
+
+wait
