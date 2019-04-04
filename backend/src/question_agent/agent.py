d650d90856b3d0dd455b3109c9161fd88313b0a9
Tim Shur <tim.shur@gmail.com>
2019-04-04 13:45:50 -0700

    fix flask error with importing DM_MODEL

--- backend/src/question_agent/agent.py
+++ backend/src/question_agent/agent.py
@@ -4,6 +4,9 @@ import pandas as pd
 from gensim.models.doc2vec import Doc2Vec
 from sklearn.metrics.pairwise import cosine_similarity
 
+DBOW_MODEL = 'backend/data/models/dbow/d2v_dbow'
+DM_MODEL = 'backend/data/models/dm/d2v_dm'
+
 
 def tokenize(sentence):
     return re.sub('[!@#$.?,;]', '', sentence).lower().split(' ')
@@ -14,16 +17,16 @@ def get_question(discussion):
         return "Hello"
     return "Subsequent conversation"
 
-
 class QuestionAgent:
-    def __init__(self):
-        print('Initializing QuestionAgent... ')
-        self.pre_trained_DBOW = Doc2Vec.load('backend/data/models/dbow/d2v_dbow')
+    def __init__(self, model_path):
+        print('Initializing QuestionAgent... ', end='')
+        self.model = Doc2Vec.load(model_path)
         relevant = ['play', 'player', 'original', 'modern']
         self.translation = pd.read_csv('backend/data/translation.csv')[relevant].dropna()
 
         self.translation['modern_token'] = self.translation.modern.apply(tokenize)
-        self.translation['modern_embed'] = self.translation.modern_token.apply(self.pre_trained_DBOW.infer_vector)
+        self.translation['modern_embed'] = self.translation.modern_token.apply(
+            self.model.infer_vector)
 
     def quote(self, session_data):
         if session_data is None:
@@ -31,7 +34,9 @@ class QuestionAgent:
 
         last_message = session_data['discussion'][-1]['text']
 
-        user_embed = self.pre_trained_DBOW.infer_vector(tokenize(last_message))
-        similarity = cosine_similarity(np.array(self.translation.modern_embed.values.tolist()), user_embed.reshape(1, -1))
+        user_embed = self.model.infer_vector(tokenize(last_message))
+        similarity = cosine_similarity(
+            np.array(self.translation.modern_embed.values.tolist()),
+            user_embed.reshape(1, -1))
 
         return self.translation.iloc[np.argmax(similarity)]['original']
