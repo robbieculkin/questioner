8429360fe1a354a861f032b26e05a5de50b1c4e0
robbieculkin <robbieculkin@gmail.com>
2019-05-04 22:15:41 -0700

    backend: [agent] character cooccurrence

--- backend/src/question_agent/agent.py
+++ backend/src/question_agent/agent.py
@@ -9,6 +9,7 @@ from sklearn.metrics.pairwise import cosine_similarity
 DBOW_MODEL = 'backend/data/models/dbow/d2v_dbow'
 DM_MODEL = 'backend/data/models/dm/d2v_dm'
 
+
 def check_none(msg=None):
     def check_none_decorator(func):
         @wraps(func)
@@ -20,28 +21,35 @@ def check_none(msg=None):
         return function_wrapper
     return check_none_decorator
 
+
 def tokenize(sentence):
     return re.sub('[!@#$.?,;]', '', sentence).lower().split(' ')
+
 
 @check_none('Hello')
 def get_question(discussion):
     """Given a discussion, creates the next question to ask."""
     return "Subsequent conversation"
 
+
 class QuestionAgent:
     def __init__(self, model_path):
         print('Initializing QuestionAgent... ', end='')
         # self.model = Doc2Vec.load(model_path)
         relevant = ['play', 'player', 'original', 'modern']
-        self.translation = pd.read_csv('backend/data/translation.csv')[relevant].dropna()
+        self.translation = pd.read_csv(
+            'backend/data/translation.csv')[relevant].dropna()
 
-        self.translation['modern_token'] = self.translation.modern.apply(tokenize)
+        self.translation['modern_token'] = self.translation.modern.apply(
+            tokenize)
         # self.translation['modern_embed'] = self.translation.modern_token.apply(
         #     self.model.infer_vector)
 
         self.templates = pd.read_csv('backend/data/templates.csv')
         with open('backend/data/lines_spoken.pkl', 'rb') as infile:
             self.lines_spoken = pickle.load(infile)
+        self.cooccurrence = pd.read_csv('backend/data/character_cooccurrence.csv',
+                                        index_col='Unnamed: 0')
 
         self.play_names = {
             "All's Well That Ends Well": "Alls well that ends well",
@@ -52,7 +60,7 @@ class QuestionAgent:
             "Cymbeline": "Cymbeline",
             "Hamlet": "Hamlet",
             "Henry IV, Part 1": "Henry IV",
-            "Henry IV Part 2": "Henry IV", #shaky on this one
+            "Henry IV Part 2": "Henry IV",  # shaky on this one
             "Henry V": "Henry V",
             "Henry VI Part 1": "Henry VI Part 1",
             "Henry VI Part 2": "Henry VI Part 2",
@@ -83,15 +91,25 @@ class QuestionAgent:
             "The Winter's Tale": "A Winters Tale"
         }
 
-    def __choose_character(self, play, n_chars):
-        #Build probabilities
+    def __choose_character(self, play):
+        # Build probabilities
         play_lines = self.lines_spoken[play]
         total_lines = sum(spoken for spoken in play_lines.values())
-        probs = {player: (play_lines[player] / total_lines) for player in play_lines}
+        probs = {player: (play_lines[player] / total_lines)
+                 for player in play_lines}
 
-        choice_idx = np.random.choice(len(play_lines), size=n_chars, p=list(probs.values()), replace=False)
-        choices = [list(probs.keys())[idx].title() for idx in choice_idx]
-        return choices
+        # anchor character: TODO choose given conversation context,
+        # or add scaling factor to avoid unimportant characters
+        anchor_char_idx = np.random.choice(len(play_lines),
+                                           size=1,
+                                           p=list(probs.values()),
+                                           replace=False)
+        anchor_char = list(probs.keys())[anchor_char_idx[0]]
+
+        # supporting character: could do this in a slightly more sophisiticated way
+        support_char = self.cooccurrence[anchor_char].idxmax()
+
+        return anchor_char.title(), support_char.title()
 
     @check_none('Hello user!')
     def quote(self, session_data):
@@ -108,19 +126,28 @@ class QuestionAgent:
     @check_none('Hello')
     def character_template(self, session_data):
         play = self.play_names[session_data['selectedPlay']]
-        character_templates = self.templates[(self.templates['1'].isin(['Character', 'Major Character'])) &
-            ((self.templates['2'].isnull()) |
-            (self.templates['2'].isin(['Character', 'Major Character'])))]
+        character_templates = self.get_character_templates()
 
-        if character_templates.empty:
-            return 'No further questions.'
-
-        template = character_templates.loc[np.random.choice(character_templates.index)]
-        characters = self.__choose_character(play, 2)
+        template = character_templates.loc[np.random.choice(
+            character_templates.index)]
+        characters = self.__choose_character(play)
 
         self.templates = self.templates.drop(template.name, axis=0)
 
-        template['Question'] = re.sub(r'\[1\]', characters[0], template['Question'])
-        template['Question'] = re.sub(r'\[2\]', characters[1], template['Question'])
+        template['Question'] = re.sub(
+            r'\[1\]', characters[0], template['Question'])
+        template['Question'] = re.sub(
+            r'\[2\]', characters[1], template['Question'])
 
         return template['Question']
+
+    def get_character_templates(self):
+        return self.templates[(self.templates['1'].isin(['Character', 'Major Character'])) &
+                              ((self.templates['2'].isnull()) |
+                               (self.templates['2'].isin(['Character', 'Major Character'])))]
+
+    def response(self, session_data):
+        if self.get_character_templates().empty:
+            return 'No further questions.'
+        else:
+            return self.character_template(session_data)
