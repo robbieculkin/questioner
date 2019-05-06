6cc56032290190e5c72aa29155f012e402fe78e1
tshur <tim.shur@gmail.com>
2019-05-05 21:30:47 -0700

    caret: added custom accent caret and custom selection highlight

--- src/components/discussion_page/Entry/index.js
+++ src/components/discussion_page/Entry/index.js
@@ -1,32 +1,109 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import getCaretCoordinates from 'textarea-caret';
 import './index.scss';
+
+const getCaretPosition = () => {
+  // Return caret position measured as a character count
+  // Return -1 if there is a selection
+  const elt = document.getElementById('entryField');
+  const start = elt.selectionStart;
+  const end = elt.selectionEnd;
+
+  console.log(start, end);
+  return start === end ? end : -1;
+};
+
+const getCaretStyle = unfocused => {
+  // visibility hidden if argument provided/falsy
+  const caretPosition = getCaretPosition();
+  const elt = document.getElementById('entryField');
+
+  if (!unfocused && caretPosition >= 0) {
+    const caret = getCaretCoordinates(elt, caretPosition);
+    const rect = elt.getBoundingClientRect();
+    // const characterWidth = 16.97;
+    // const lineHeight = 31;
+    // const charactersPerLine = Math.floor((rect.right - rect.left) / characterWidth + 1);
+    // const horizontalOffset = characterWidth * (caretPosition % charactersPerLine);
+    // const verticalOffset = lineHeight * Math.floor(caretPosition / charactersPerLine);
+
+    return {
+      top: rect.top + caret.top,
+      left: rect.left + caret.left,
+      visibility: 'visible'
+    };
+  } else {
+    return {
+      visibility: 'hidden'
+    };
+  }
+};
 
 class Entry extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      value: ''
+      value: '',
+      caretStyle: {
+        visibility: 'hidden'
+      }
     };
 
+    this.handleClick = this.handleClick.bind(this);
+    this.handleScroll = this.handleScroll.bind(this);
+    this.handleFocus = this.handleFocus.bind(this);
+    this.handleBlur = this.handleBlur.bind(this);
     this.handleChange = this.handleChange.bind(this);
-    this.handleKeyPress = this.handleKeyPress.bind(this);
+    this.handleKeyDown = this.handleKeyDown.bind(this);
+    this.handleKeyUp = this.handleKeyUp.bind(this);
     this.handleTextEntry = this.handleTextEntry.bind(this);
   }
 
-  handleChange(event) {
-    this.setState({ value: event.target.value });
+  componentDidMount() {
+    window.addEventListener('scroll', this.handleScroll);
   }
 
-  handleKeyPress(event) {
+  componentWillUnmount() {
+    window.removeEventListener('scroll', this.handleScroll);
+  }
+
+  handleClick() {
+    this.setState({ caretStyle: getCaretStyle() });
+  }
+
+  handleScroll() {
+    this.setState({ caretStyle: getCaretStyle() });
+  }
+
+  handleFocus() {
+    this.setState({ caretStyle: getCaretStyle() });
+  }
+
+  handleBlur() {
+    this.setState({ caretStyle: getCaretStyle('unfocused') });
+  }
+
+  handleChange(event) {
+    this.setState({
+      value: event.target.value,
+    });
+  }
+
+  handleKeyDown(event) {
     // Submit textentry when <ENTER> is pressed (instead of newline)
     const { value } = this.state;
+    this.setState({ caretStyle: getCaretStyle() });
 
     if (event.key === 'Enter') {
       event.preventDefault();
       this.handleTextEntry(value);
     }
+  }
+
+  handleKeyUp() {
+    this.setState({ caretStyle: getCaretStyle() });
   }
 
   handleTextEntry(value) {
@@ -35,11 +112,14 @@ class Entry extends Component {
       return;
 
     onTextEntry(value);
-    this.setState({ value: '' });
+    this.setState({
+      value: '',
+      caretStyle: getCaretStyle()
+    });
   }
 
   render() {
-    const { value } = this.state;
+    const { value, caretStyle } = this.state;
 
     return (
       <div className='entry'>
@@ -49,8 +129,13 @@ class Entry extends Component {
           autoFocus
           value={value}
           onChange={this.handleChange}
-          onKeyPress={this.handleKeyPress}
+          onKeyDown={this.handleKeyDown}
+          onKeyUp={this.handleKeyUp}
+          onClick={this.handleClick}
+          onFocus={this.handleFocus}
+          onBlur={this.handleBlur}
         />
+        <div className='caret' style={caretStyle}></div>
       </div>
     );
   }
